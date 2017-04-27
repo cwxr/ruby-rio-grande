915c45aa98c1010ed5a71c12efbb19545e5ee2f8
robin chua <robinchua@gmail.com>
2017-04-27 09:23:03 +0800

    latest commit

--- spec/bluray_spec.rb
+++ spec/bluray_spec.rb
@@ -4,10 +4,45 @@ require_relative '../lib/bluray'
 describe Bluray do
   before(:context) do
     # initialize item
+    @blu1 = Bluray.new("Men In Black", 29.00, 110, 'Barry Sonnenfeld', 'Will Smith')
   end
 
   # check initialization
+  describe 'Initialization' do
   # check that it is an extended from Item
+    it 'should be a kind of Item' do
+      expect(@blu1).to be_kind_of(Item)
+    end
   # check that it is an instance of Bluray
+  it 'should be an instance of Bluray' do
+      expect(@blu1).to be_instance_of(Bluray)
+    end
+  end
   # check getters and setters
+  describe 'getters and setters' do
+    it 'assigns and reads the name' do
+      @blu1.name = 'Men In Black'
+      expect(@blu1.name).to eq('Men In Black')
+    end
+
+    it 'assigns and reads the price' do
+      @blu1.price = 29.00
+      expect(@blu1.price).to eq(29.00)
+    end
+
+    it 'assigns and reads the run time' do
+      @blu1.run_time = 110
+      expect(@blu1.run_time).to eq(110)
+    end
+
+    it 'assigns and reads the director' do
+      @blu1.director = 'Barry Sonnenfeld'
+      expect(@blu1.director).to eq('Barry Sonnenfeld')
+    end
+
+    it 'assigns and reads the producer' do
+      @blu1.producer = 'Will Smith'
+      expect(@blu1.producer).to eq('Will Smith')
+    end
+  end
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -1,13 +1,52 @@
 require_relative 'spec_helper'
 require_relative '../lib/cd'
 
-describe Cd do
+describe 'Cd' do
   before(:context) do
     # initialize item
+    @cd1 = Cd.new('Kenny G Best Hits', 25.00, 10, 'Kenny G', 60)
   end
 
   # check initialization
-  # check that it is an extended from Item
-  # check that it is an instance of Cd
+  describe 'Initialization' do
+
+    # check that it is an instance of Cd
+    it 'should be a cd' do
+      expect(@cd1).to be_instance_of(Cd)
+    end
+
+    # check that it is an extended from Item
+    it 'should be an extension of Item' do
+      expect(@cd1).to be_kind_of(Item)
+    end
+  end
+
   # check getters and setters
+  describe 'getters and setters' do
+    it 'assigns and reads the name' do
+      @cd1.name = 'Kenny G Best Hits'
+      expect(@cd1.name).to eq('Kenny G Best Hits')
+    end
+
+    it 'assigns and reads the price' do
+      @cd1.price = 25.00
+      expect(@cd1.price).to eq(25.00)
+    end
+
+    it 'assigns and reads the number of tracks' do
+      @cd1.tracks = 10
+      expect(@cd1.tracks).to eq(10)
+    end
+
+    it 'assigns and reads the artist' do
+      @cd1.artist = 'Kenny G'
+      expect(@cd1.artist).to eq('Kenny G')
+    end
+
+    it 'assigns and reads the run_time' do
+      @cd1.run_time = 60
+      expect(@cd1.run_time).to eq(60)
+    end
+
+  end
 end
